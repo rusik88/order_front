a7c9c2162ce39668cbe913304a0ea07126be67b6
rusik88 <rusik.bespalov54@gmail.com>
2026-06-05 13:10:18 +0300

    Configured Eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,59 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+import importPlugin from "eslint-plugin-import";
+import prettier from "eslint-config-prettier";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettier,
+
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      globals: globals.browser,
+    files: ["**/*.{js,ts,jsx,tsx}"],
+
+    plugins: {
+      import: importPlugin,
     },
+
+    rules: {
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "no-debugger": "warn",
+
+      "semi": ["error", "always"],
+      "quotes": ["error", "double"],
+      "no-unexpected-multiline": "error",
+
+      "import/order": [
+        "warn",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+          ],
+          "newlines-between": "always",
+        },
+      ],
+
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+
+      // "@typescript-eslint/consistent-type-imports": "warn",
+    },
+
+    ignores: [
+      "dist/**",
+      "build/**",
+      "node_modules/**",
+    ],
   },
-])
+]);
