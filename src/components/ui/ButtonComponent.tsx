690e6c6a68a10fc3d55939b9007c1cebf027abd5
rusik88 <rusik.bespalov54@gmail.com>
2026-06-06 10:26:01 +0300

    Added disabled to buttonComponent

--- src/components/ui/ButtonComponent.tsx
+++ src/components/ui/ButtonComponent.tsx
@@ -1,14 +1,16 @@
 import clsx from 'clsx';
-import type {ReactNode} from 'react';
+import type { ReactNode } from 'react';
 
 const ButtonComponent = (
-    { children, classNames }
-    : {children: ReactNode, classNames?: string}) => {
+    { children, classNames, isDisabled }
+    : {children: ReactNode, classNames?: string, isDisabled?: boolean}) => {
     return (
       <button className={clsx(
-          classNames,
-          'cursor-pointer h-13 w-full rounded-xl bg-linear-to-r from-blue-500 to-blue-600 text-xl font-semibold text-white transition hover:from-blue-400 hover:to-blue-500'
-      )}>{ children }</button>
+            classNames,
+            'h-13 w-full rounded-xl bg-linear-to-r from-blue-500 to-blue-600 text-xl font-semibold text-white transition',
+            'hover:from-blue-400 hover:to-blue-500 cursor-pointer',
+            'disabled:cursor-not-allowed disabled:opacity-60 disabled:from-blue-500 disabled:to-blue-600'
+      )} disabled={isDisabled}>{ children }</button>
     );
 };
 
